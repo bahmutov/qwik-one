387b17984787aeb61be1e76c032b725f25ca3482
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-12 08:41:23 -0400

    solution

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,33 +1,29 @@
 /// <reference types="cypress" />
 
 it('downloads the click handler to show the alert', () => {
-  // get the window object and stub the alert method
-  // and give it an alias "windowAlert"
-  // Tip: cy.visit yields the window object
-  // https://on.cypress.io/cy/visit
-  // https://on.cypress.io/stub
-  // https://on.cypress.io/as
   cy.visit('/').then((win) => {
     cy.stub(win, 'alert').as('windowAlert')
   })
-  // spy on the JS download using cy.intercept command
-  // Tip: get the JS file URL from the "on:click" attribute
-  // of the "Click me" button
-  // https://on.cypress.io/intercept
-  // Note: the click handler resource includes "<pathname>#<random hash>"
-  // so parse it to get just the pathname to call cy.intercept
   cy.contains('button', 'Click me')
     .as('button')
     .should('have.attr', 'on:click')
     .should('be.a', 'string')
     .then((clickResource) => {
       const pathname = clickResource.split('#')[0]
-      cy.intercept({ method: 'GET', pathname }).as('clickHandler')
+      cy.intercept({ method: 'GET', pathname }, cy.stub().as('download'))
     })
-  // click on the "Click me" button
+  // Can you confirm that the click JavaScript resource
+  // was downloaded _before_ the "alert" was called?
+  // Tip: Sinon.js and Sinon-Chai has assertions to confirm
+  // if one spy was called before another spy
+  // https://sinonjs.org/
+  // https://www.chaijs.com/plugins/sinon-chai/
   cy.get('@button').click()
-  // confirm the "alert" was called with the string "42"
-  cy.get('@windowAlert').should('have.been.calledWith', '42')
-  // confirm the JS resource was requested
-  cy.wait('@clickHandler')
+  cy.get('@download')
+    .should('have.been.calledOnce')
+    .then((download) => {
+      cy.get('@windowAlert')
+        .should('have.been.calledWith', '42')
+        .and('be.calledAfter', download)
+    })
 })
